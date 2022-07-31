251d57b4a282d82da0622adcff9051ec0165a1fc
Mohamed Saleem <mohamedselim701@gmail.com>
2022-07-31 22:53:30 +0200

    python - easy - finding-the-percentage

---
+++ Hackerrank/python/finding-the-percentage.py
@@ -0,0 +1,11 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+print("{:.2f}".format(sum(student_marks[query_name])/len(student_marks[query_name])))
+print(len(student_marks[query_name]))
+print(student_marks[query_name])
+print(sum(student_marks[query_name]))
