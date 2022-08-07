8716055a62df0847fd59cb3fef30280152cdaa5d
_Saleem_ <mohamedselim701@gmail.com>
2022-08-07 19:32:53 +0200

    python - esay - python-lists

---
+++ Hackerrank/python/python-lists.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/python-lists/problem?isFullScreen=true
+# easy
+
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+    for i in range(N):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd != 'print':
+            cmd += "(" + ",".join(args) + ")"
+            eval("lst." + cmd)
+        else:
+            print(lst)
+        
