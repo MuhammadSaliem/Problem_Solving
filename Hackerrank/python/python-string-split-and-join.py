b9845d6c4a15c7b98139d9a41d090b18f47613d8
_Saleem_ <mohamedselim701@gmail.com>
2022-08-07 19:36:55 +0200

    python - string - easy - python-string-split-and-join

---
+++ Hackerrank/python/python-string-split-and-join.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+# string
+# easy
+
+def split_and_join(line):
+    return '-'.join(line.split(" "))
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
